1d5c709e0e28c58f0c3f261bdefcd5c0678de774
MarioOlla <mario.olla25@gmail.com>
2022-12-01 20:01:51 +0100

    prima implementazione delle classi di modello

---
+++ src/main/java/it/prova/triage/model/Paziente.java
@@ -0,0 +1,94 @@
+package it.prova.triage.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paziente")
+public class Paziente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "codiceFiscale")
+	private String codiceFiscale;
+	@Column(name = "dataRegistrazione")
+	private LocalDate dataRegistrazione;
+	@Column(name = "stato")
+	private StatoPaziente stato;
+
+	public Paziente() {
+		super();
+	}
+
+	public Paziente(Long id, String nome, String cognome, String codiceFiscale, LocalDate dataRegistrazione,
+			StatoPaziente stato) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.codiceFiscale = codiceFiscale;
+		this.dataRegistrazione = dataRegistrazione;
+		this.stato = stato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public LocalDate getDataRegistrazione() {
+		return dataRegistrazione;
+	}
+
+	public void setDataRegistrazione(LocalDate dataRegistrazione) {
+		this.dataRegistrazione = dataRegistrazione;
+	}
+
+	public StatoPaziente getStato() {
+		return stato;
+	}
+
+	public void setStato(StatoPaziente stato) {
+		this.stato = stato;
+	}
+
+}
